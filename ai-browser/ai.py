8f8ade7e562c5ff56f94c7107dd04e587775d3db
Ayush <>
2025-05-17 17:54:54 +0530

    vault backup: 2025-05-17 17:54:54

--- ai-browser/ai.py
+++ ai-browser/ai.py
@@ -1,13 +1,20 @@
 from langchain_ollama import ChatOllama
 from browser_use import Agent
 from pydantic import SecretStr
-
+import asyncio
 
 # Initialize the model
 llm=ChatOllama(model="qwen2.5", num_ctx=32000)
 
 # Create agent with the model
-agent = Agent(
-    task="Compare the price of gpt-4o and DeepSeek-V3",
-    llm=llm
-)
+async def main():
+    agent = Agent(
+        task="Compare the price of gpt-4o and DeepSeek-V3",
+        llm=llm
+    )
+
+
+    result = await agent.run()
+    print(result)
+
+asyncio.run(main())
