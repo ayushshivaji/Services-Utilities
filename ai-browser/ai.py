cf188ce3975ba4bb3e60c88724fbe697628fcdac
Ayush <>
2025-05-17 17:33:41 +0530

    vault backup: 2025-05-17 17:33:41

---
+++ ai-browser/ai.py
@@ -0,0 +1,13 @@
+from langchain_ollama import ChatOllama
+from browser_use import Agent
+from pydantic import SecretStr
+
+
+# Initialize the model
+llm=ChatOllama(model="qwen2.5", num_ctx=32000)
+
+# Create agent with the model
+agent = Agent(
+    task="Compare the price of gpt-4o and DeepSeek-V3",
+    llm=llm
+)
